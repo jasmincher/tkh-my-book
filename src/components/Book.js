8082b7971d7b1db934dac920ea2a881a1fda3f52
Jas <c.jasminher@gmail.com>
2022-01-04 23:13:18 -0500

    Aligned desc text to left

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,20 +1,17 @@
 import React from "react";
-import '../App.css'
+import "../App.css";
 
-function Book(props){
+function Book(props) {
+  return (
+    <div className="book-container">
+      <h1 className="book-title">{props.title}</h1>
 
-    return (
-        <div className="book-container">
-            <h2 className="book-title">{props.title}</h2>
-            <div className="book-info">
-
-            <img src={props.img} className="book-img"/>
-            <p className="book-desc">{props.desc}</p>
-
-            </div>
-        </div>
-    )
-
+      <div className="book-info">
+        <img src={props.img} className="book-img" />
+        <p className="book-desc">{props.desc}</p>
+      </div>
+    </div>
+  );
 }
 
 export default Book;
